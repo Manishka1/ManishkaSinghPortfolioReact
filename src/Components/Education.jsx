f2fbe0674d5ec5acf90e56b7cbe15339e47e113d
Manishka Singh <87705011+Manishka1@users.noreply.github.com>
2024-07-13 17:54:44 +0000

    ManishkaSinghPortfolioReact

---
+++ src/Components/Education.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Education = () => {
+  return (
+    <section className="padding" id="education">
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "50%",
+          padding: "4rem",
+          margin: "3rem auto",
+          textAlign: "center",
+        }}
+      >
+        <h2>Education</h2>
+        <p>
+          I am currently pursuing my Masters in Computer Applicaton from Institute of Engineering and Technology, Lucknow.
+        </p>
+      </div>
+    </section>
+  );
+}
